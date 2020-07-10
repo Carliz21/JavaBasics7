b4330bfa0692bf28702c93ad67c6aea6008369d7
Carliz21 <carmenlizferrufino@gmail.com>
2020-07-09 21:14:47 -0400

    Accessing Modifiers Demo

--- com/syntax/class19/AccessingEmployeeMembers.java
+++ com/syntax/class19/AccessingEmployeeMembers.java
@@ -1,5 +1,22 @@
 package com.syntax.class19;
 
 public class AccessingEmployeeMembers {
+	
+	public static void main(String[] args) {
+		
+		Employee emp= new Employee();
+		emp.name= "John";
+		emp.lastName= "Smith";
+		emp.age= 35;
+		emp.salary=90000;
+		//emp.ssn--> wont be available because it's private 
+		
+		emp.displayName();
+		emp.displayAge();
+		emp.displaySalary();
+		//emp.displaySsn(); --> won't be available because it's private 
+		
+		
+	}
 
 }
